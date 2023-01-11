fe159442631684df4fd5a03661c21579a0febd84
Khalid Najmeddine <knajm574@gmail.com>
2023-01-11 12:59:30 -0700

    feat: floating point values

--- introductory-rust-project/src/main.rs
+++ introductory-rust-project/src/main.rs
@@ -4,7 +4,18 @@ fn main() {
     let first_name = "John";
     println!("Your name is {}", first_name);
 
-    let age = 30u8;
+    let age = 30;
     println!("Your age is {}", age);
+
+    let population = 62000000;
+    println!("The population of the city is {}", population);
+
+    let red = 0xFA;
+    println!("Red is {:x}", red);
+    let rgb = 0xFF0000;
+    println!("RGB is {:x}", rgb);
+
+    let distance_kilometers = 5.5;
+    println!("The distance is {} kilometers", distance_kilometers);
 
 }
